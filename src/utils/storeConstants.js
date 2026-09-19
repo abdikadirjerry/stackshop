0bfbebd54c86169f4c90415107fc640305ef178b
Abdikadir Jerry <abdikadirj.code@gmail.com>
2026-09-19 23:27:13 +0300

    refactor: centralize store constants

---
+++ src/utils/storeConstants.js
@@ -0,0 +1,7 @@
+export const SHIPPING_COST = 10;
+
+export const CART_STORAGE_KEY = "stackshop-cart";
+
+export const WISHLIST_STORAGE_KEY = "stackshop-wishlist";
+
+export const ORDER_STORAGE_KEY = "stackshop-last-order";
